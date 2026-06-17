977b8bffda51300cf78389f33c601eccc1ec3326
tacman <tacman@users.noreply.github.com>
2026-06-17 12:11:17 +0000

    better kit-bundle integration

--- src/SurvosMobileBundle.php
+++ src/SurvosMobileBundle.php
@@ -13,6 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Reference;
+// Symfony\Component\HttpKernel\Bundle\Bundle <-- Flex auto-registration marker (see Survos\Kit\AbstractSurvosBundle)
 class SurvosMobileBundle extends AbstractUxBundle implements CompilerPassInterface
 {
     public const ASSET_PACKAGE = 'mobile';
